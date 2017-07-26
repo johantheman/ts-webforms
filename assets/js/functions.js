0a116e288d010f00b2f1375e79d1d9b077904591
Johan Erasmus <johan@vine.co.za>
2017-07-26 10:29:32 +0200

    added the html prototyp form

---
+++ assets/js/functions.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Johan on 7/26/2017.
+ */
+function addFields(){
+    var number = document.getElementById("member").value;
+    var container = document.getElementById("container");
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    for (i=0;i<number;i++){
+        container.appendChild(document.createTextNode("Friend " + (i+1) + ":"));
+        var input = document.createElement("input");
+        input.type = "text";
+        container.appendChild(input);
+        container.appendChild(document.createElement("br"));
+    }
+    var button = document.createElement("input");
+    button.type = "submit";
+    button.value = "Click to add"
+    container.appendChild(document.createElement("br"));
+    container.appendChild(button);
+}
